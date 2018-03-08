ba52bd17bcd68b978814f6e951dc001eb614f923
odedhutzler <oded.hutzler@kaltura.com>
2018-03-08 08:45:29 +0200

    creating a download item instead of just a json

---
+++ src/item/download-item.js
@@ -0,0 +1,22 @@
+export class DownloadItem {
+  constructor(data){
+
+  }
+
+  get status(){
+    return this.status;
+  }
+
+  set status(status){
+    this.status = status;
+  }
+
+  /**
+   * override probably
+   */
+  prepareItemForStorage(){
+
+  }
+
+
+}
