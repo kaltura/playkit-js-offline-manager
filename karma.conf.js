0a59084dfdc325d44cd81858ed34226c878f8d7e
Gilad Nadav <gilad.nadav@kaltura.com>
2023-04-18 08:49:15 +0300

    github actions canary

--- karma.conf.js
+++ karma.conf.js
@@ -11,12 +11,10 @@ webpackConfig.resolve.alias = {
   'playkit-js-providers': path.resolve('./node_modules/playkit-js-providers/dist/playkit-ovp-provider')
 };
 
-const isWindows = /^win/.test(process.platform);
-const isMacOS = /^darwin/.test(process.platform);
 // Create custom launcher in case running with Travis
-const customLaunchers = {
-  Chrome_travis_ci: {
-    base: 'Chrome',
+const launchers = {
+  Chrome_browser: {
+    base: 'ChromeHeadless',
     flags: ['--no-sandbox', '--autoplay-policy=no-user-gesture-required']
   }
 };
@@ -24,10 +22,7 @@ const customLaunchers = {
 module.exports = function (config) {
   let karmaConf = {
     logLevel: config.LOG_INFO,
-    browsers: [
-      'Chrome',
-      'Firefox'
-    ],
+    browsers: [],
     concurrency: 1,
     singleRun: true,
     colors: true,
@@ -63,18 +58,7 @@ module.exports = function (config) {
     }
   };
 
-  if (process.env.TRAVIS) {
-    karmaConf.customLaunchers = customLaunchers;
-    karmaConf.browsers = [
-      'Chrome_travis_ci'
-    ];
-  } else {
-    if (isWindows) {
-      karmaConf.browsers.push('IE');
-    } else if (isMacOS) {
-      karmaConf.browsers.push('Safari');
-    }
-  }
-
+  karmaConf.customLaunchers = launchers;
+  karmaConf.browsers = ['Chrome_browser'];
   config.set(karmaConf);
 };
